52b69da9f1530d42efdd0fcf29807760f0a1e6c3
aditya <agoswami0708g@gmail.com>
2022-11-03 13:16:21 +0530

    done

--- block-BRaadi/code/src/components/SectionTwo.js
+++ block-BRaadi/code/src/components/SectionTwo.js
@@ -11,10 +11,10 @@ class SectionTwo extends React.Component {
         <p className="small">
           Clicking this button will call a function. Inside that function an
           error is thrown. That will make the component gets unmounted. Fix it
-          using error boundary. Make this component display `An error occured!`
+          using error boundary. Make this component display `An error occured!`    
         </p>
         <button className="error" onClick={this.handleClick}>
-          Click to Throw an Error
+          Click to Throw an Error          
         </button>
       </div>
     );
